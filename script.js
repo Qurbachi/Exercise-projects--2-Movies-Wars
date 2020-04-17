5f3ff53f27acb19384602ca57a490c2dace556ad
Damian Tworzydło <damian.tworzydlo17@gmail.com>
2020-04-17 12:39:43 +0200

    Creates debouncing function for input search

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '13e3bac1',
-            s: 'Shrek'
+            s: searchTerm
         }
     });
 
     console.log(response);
 };
 
-fetchData();
+const input = document.querySelector('input');
 
+let timeoutId;
+const onInput = event => {
+    if(timeoutId) {
+        clearInterval(timeoutId);
+    }
+    timeoutId = setTimeout( () => {fetchData(event.target.value)}, 1000);
+};
+
+input.addEventListener('input', onInput);
